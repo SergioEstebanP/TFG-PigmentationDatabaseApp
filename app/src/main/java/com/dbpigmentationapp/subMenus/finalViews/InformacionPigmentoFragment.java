22335c81e64b5ffed1fcf19a9edd273487ee4b63
Sergio Esteban <sergi2312@gmail.com>
2019-05-12 19:02:45 +0200

    Pantalla Principal para mostrar información de los pigmentos acabada

--- app/src/main/java/com/dbpigmentationapp/subMenus/finalViews/InformacionPigmentoFragment.java
+++ app/src/main/java/com/dbpigmentationapp/subMenus/finalViews/InformacionPigmentoFragment.java
@@ -5,35 +5,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dbpigmentationapp.R;
 
 public class InformacionPigmentoFragment extends Fragment {
 
+    private ImageView colorimetria;
+    private TextView descColorimetria;
+    private Animation animationUp;
+    private Animation animationDown;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_informacion_pigmento, container, false);
 
-//        title = view.findViewById(R.id.title);
-//        content = view.findViewById(R.id.content);
-//        content.setVisibility(View.GONE);
-//
-//        animationUp = AnimationUtils.loadAnimation(view.getContext(), R.anim.slide_up);
-//        animationDown = AnimationUtils.loadAnimation(view.getContext(), R.anim.slide_down);
-//
-//        title.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(content.isShown()){
-//                    content.setVisibility(View.GONE);
-//                    content.startAnimation(animationUp);
-//                }
-//                else{
-//                    content.setVisibility(View.VISIBLE);
-//                    content.startAnimation(animationDown);
-//                }
-//            }
-//        });
+        colorimetria = view.findViewById(R.id.imgColorimetria);
+        descColorimetria = view.findViewById(R.id.descColorimetria);
+        descColorimetria.setVisibility(View.GONE);
+
+        animationUp = AnimationUtils.loadAnimation(view.getContext(), R.anim.slide_up);
+        animationDown = AnimationUtils.loadAnimation(view.getContext(), R.anim.slide_down);
+
+        colorimetria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(descColorimetria.isShown()){
+                    descColorimetria.setVisibility(View.GONE);
+                    descColorimetria.startAnimation(animationUp);
+                }
+                else{
+                    descColorimetria.setVisibility(View.VISIBLE);
+                    descColorimetria.startAnimation(animationDown);
+                }
+            }
+        });
 
         return view;
     }
