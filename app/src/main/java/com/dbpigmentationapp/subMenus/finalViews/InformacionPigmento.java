bda416a4ae09640addbd161f2f3a520d3e6c7dd1
Sergio Esteban <sergi2312@gmail.com>
2019-05-12 17:36:45 +0200

    Conseguido mostrar desplegables en la aplicación

--- app/src/main/java/com/dbpigmentationapp/subMenus/finalViews/InformacionPigmento.java
+++ app/src/main/java/com/dbpigmentationapp/subMenus/finalViews/InformacionPigmento.java
@@ -1,7 +1,7 @@
 package com.dbpigmentationapp.subMenus.finalViews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.dbpigmentationapp.R;
 
---
+++ app/src/main/java/com/dbpigmentationapp/subMenus/finalViews/InformacionPigmentoFragment.java
@@ -0,0 +1,53 @@
+package com.dbpigmentationapp.subMenus.finalViews;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.dbpigmentationapp.R;
+
+public class InformacionPigmentoFragment extends Fragment {
+
+    private TextView title;
+    private TextView content;
+    private Animation animationUp;
+    private Animation animationDown;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_informacion_pigmento, container, false);
+
+        title = view.findViewById(R.id.title);
+        content = view.findViewById(R.id.content);
+        content.setVisibility(View.GONE);
+
+        animationUp = AnimationUtils.loadAnimation(view.getContext(), R.anim.slide_up);
+        animationDown = AnimationUtils.loadAnimation(view.getContext(), R.anim.slide_down);
+
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(content.isShown()){
+                    content.setVisibility(View.GONE);
+                    content.startAnimation(animationUp);
+                }
+                else{
+                    content.setVisibility(View.VISIBLE);
+                    content.startAnimation(animationDown);
+                }
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
