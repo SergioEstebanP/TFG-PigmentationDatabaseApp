48d2f801b752a3aeae9f8f8674cd59d107577bdd
Sergio Esteban <sergio.esteban@externals.adidas.com>
2019-07-05 15:47:03 +0200

    betterui in table

--- app/src/main/java/com/dbpigmentationapp/subMenus/ConsultaElemento.java
+++ app/src/main/java/com/dbpigmentationapp/subMenus/ConsultaElemento.java
@@ -38,10 +38,10 @@ public class ConsultaElemento extends AppCompatActivity {
 
 
         TabLayout tabLayout = findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+        tabLayout.addTab(tabLayout.newTab().setText(""));
+        tabLayout.addTab(tabLayout.newTab().setText(""));
+        tabLayout.addTab(tabLayout.newTab().setText(""));
+        tabLayout.addTab(tabLayout.newTab().setText(""));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = findViewById(R.id.pager);
